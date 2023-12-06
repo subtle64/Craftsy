2961f3d16b8ce388a5fc0385d6021297b82dcba4
Jason Adriel <143072575+subtle64@users.noreply.github.com>
2023-12-07 00:38:27 +0700

    Fix stuff.

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -8,11 +8,14 @@
 
 @section('content')
 
-<div class="d-flex flex-col mt-5 p-16">
-    <h2 style="color:rgb(141,123,104); font-family:Abhaya Libre ExtraBold;font-weight: 800; font-size:36px;">{{ $search }}</h2>
-    <p>{{ $error }}</p>
-    @foreach($items as $i)
-        <a href="#" class = "m-2" style = "text-decoration: none; width: 125px;">
+<div class="d-flex flex-column mt-5 p-5">
+    <div class = "d-flex flex-column">
+        <h2 style="color:rgb(141,123,104); font-family:Abhaya Libre ExtraBold;font-weight: 800; font-size:36px;">Search results for: <u>{{ $search }}</u></h2>
+        <p style="font-weight: 300; font-size:20px;">{{ $error }}</p>
+    </div>
+    <div class = "d-flex">
+        @foreach($items as $i)
+        <a href="#" class="m-2" style="text-decoration: none; width: 15%;">
             @component('components.product_card')
             @slot('image')
             {{ $i->image_path }}
@@ -29,6 +32,7 @@
             @endcomponent
         </a>
         @endforeach
+    </div>
 </div>
 
 @endsection
